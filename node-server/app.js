eee48abc73bb3c7b7ef87a045f6b97bc43ac83c0
Kapil Raghuwanshi <Kapil.Raghuwanshi5@gmail.com>
2021-08-21 19:19:06 +0530

    Weather: Added fetch call to node API

--- node-server/app.js
+++ node-server/app.js
@@ -5,22 +5,28 @@ const port = 3001;
 
 const API_KEY = "d37f78971b1063ccf34a287df290899e";
 
-const URL = "https://api.openweathermap.org/data/2.5/weather?" ;
+const URL = "https://api.openweathermap.org/data/2.5/onecall?";
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/getWeatherInfo/', async (req, resp) => {
     const lat = req.query.lat;
     const lon = req.query.lon;
-    console.log(lat, lon);
+    // console.log(lat, lon);
     let result;
     // https://api.openweathermap.org/data/2.5/onecall?lat=10&lon=80&appid=d37f78971b1063ccf34a287df290899e
     try {
-        result = await axios.get(URL+"lat="+lat+"&lon="+lon+"&appid="+API_KEY);
-    } catch(err){
+        result = await axios.get(URL + "lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY);
+    } catch (err) {
         console.error(`failed due to ${err.message}`);
     }
     resp.json(result.data);
 });
 
 app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
+    console.log(`Express app listening at http://localhost:${port}`);
 })
--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,17 +1,62 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { useAPI } from './APIHooks';
-
+import moment from 'moment';
+import myLogo from './img/weather_forecast_symbol.png';
+import Accordion from './Accordion';
+ 
 const App = () => {
 
-  const [{ data: getActionMovieData, loading: isActionMovieLoading, error: getActionMovieError }, actionRefetch] = useAPI('movies/categories?lat=true&long=action');
+  const [weatherData, setWeatherData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchWeatherDetails = async () => {
+    const response = await fetch(
+      //`http://localhost:3001/getWeatherInfo?lat=${geoLocation.lattitude}&lon=${geoLocation.longitude}`,
+      `http://localhost:3001/getWeatherInfo?lat=20&lon=30`,
+      {
+        method: "GET",
+        headers: new Headers({
+          'Accept': 'application/json',
+          'Content-Type': 'application/json;charset=utf-8'
+        })
+      }
+    );
+      const postData = await response.json();
+      setWeatherData(postData);
+      setIsLoading(false);
+    };
+    fetchWeatherDetails();
+  }, []);
+
+  console.log(weatherData);
   
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h3> React Node Weather App </h3>
+        <img src={myLogo} className="app_logo" alt="logo" />
       </header>
+      {isLoading && <p>Wait we're loading weather details for your location!</p>}
+      <h4>Today's Weather at timezone: {weatherData.timezone}</h4>
+      <table>
+        <tr>
+        <td><div>{moment(weatherData.current.dt).toDate()}</div></td>
+          <td><div>{weatherData.current.weather[0].description}</div></td>
+          <td><img src={weatherData.current.weather[0].icon} alt="icon"></img></td>
+          <td><div>{weatherData.current.humidity}</div></td>
+          <td><div>{weatherData.current.pressure}</div></td>
+          <td><div>{weatherData.current.sunrise}</div></td>
+          <td><div>{weatherData.current.sunset}</div></td>
+        </tr>
+      </table>
+     
+      {weatherData.daily && weatherData.daily.map((item, idx) => (
+        <div key={idx}>
+          <div>{item.weather[0].description}</div>
+          {/* <Accordion title={title} content={content} /> */}
+        </div>
+      ))}
     </div>
   );
 }
